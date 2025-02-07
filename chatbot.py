cf5876a0bce8eada99998a590ecafa69cd4e60a0
manav <manavjotsingh17@gmail.com>
2025-02-06 22:02:01 -0800

    added chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 
-MODEL_NAME = "google-t5/t5-large"
-# MODEL_NAME = "facebook/blenderbot-400M-distill"
+# MODEL_NAME = "google-t5/t5-large"
+MODEL_NAME = "facebook/blenderbot-400M-distill"
 CUSTOM_CACHE_DIR = "cache"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -17,12 +17,11 @@ conversation_history = []
 def chat(input_text):
     history_string = "\n".join(conversation_history)
 
-    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
-    inputs = {name: tensor.to(device) for name, tensor in inputs.items()}
+    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt").to(device)
 
     outputs = model.generate(**inputs, max_length=100)
 
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    response = tokenizer.decode(outputs[0]).strip()
 
     conversation_history.append(input_text)
     conversation_history.append(response)
